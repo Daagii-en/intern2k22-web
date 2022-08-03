54b0a65666f31d42115c6fc1f6bb8a66d695ab42
Daagii-en <82932555+Daagii-en@users.noreply.github.com>
2022-08-03 13:50:15 +0800

    add

--- intern2022/src/components/Footer.jsx
+++ intern2022/src/components/Footer.jsx
@@ -61,21 +61,27 @@ export default class Footer extends Component {
                 </a>
               </li>
               <li className="col">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
-                  fill="currentColor"
-                  className="bi bi-geo-alt icons-ft"
-                  viewBox="-5 -5 25 25"
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    fill="currentColor"
+                    className="bi bi-geo-alt icons-ft"
+                    viewBox="-5 -5 25 25"
+                  >
+                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  </svg>
+                </div>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.google.com/maps/place/Golomt+Bank/@47.9153929,106.9039839,17z/data=!3m1!4b1!4m5!3m4!1s0x5d9692505ce5aa8d:0x567dee2f413e3046!8m2!3d47.9153893!4d106.9061726"
                 >
-                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                </svg>
-                <label className="w-50">
                   Голомт банкны Төв байр, Д.Сүхбаатарын талбай 5, Ш/х-22,
                   Улаанбаатар 15160, Монгол улс
-                </label>
+                </a>
               </li>
             </ul>
           </div>
