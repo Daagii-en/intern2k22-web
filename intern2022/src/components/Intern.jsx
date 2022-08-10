b995851d9d48054f75bf1f215e570c892c62f899
Daagii-en <82932555+Daagii-en@users.noreply.github.com>
2022-08-10 10:18:17 +0800

    :alien:

--- intern2022/src/components/Intern.jsx
+++ intern2022/src/components/Intern.jsx
@@ -3,6 +3,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import davaajargal from "../logo/devImg1.jpg";
 import tsenguun from "../logo/devImg2.jpg";
+import profile from "../logo/profile.jpg";
 export function Intern() {
   useEffect(() => {
     Aos.init({ duration: 1000 });
@@ -19,16 +20,27 @@ export function Intern() {
             </div>
             <div className="text-center">
               <p>U.Davaajargal</p>
+              <p>NUM</p>
             </div>
           </div>
           <div data-aos="fade-up" className="card card__three">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
           <div data-aos="fade-up" className="card card__four"></div>
           <div data-aos="fade-up" className="card card__two bg-warning">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
         </div>
         <div className="col-2 box--holder">
@@ -36,48 +48,91 @@ export function Intern() {
             <div className="container card__img">
               <img src={tsenguun} alt="zurag" />
             </div>
-            <div className="text-center">O.Tsenguun</div>
+            <div className="text-center">
+              <p>O.Tsenguun</p>
+              <p>NUM</p>
+            </div>
           </div>
           <div data-aos="fade-up" className="card card__four"></div>
           <div data-aos="fade-up" className="card card__three bg-info">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
           <div data-aos="fade-up" className="card card__one"></div>
           <div data-aos="fade-up" className="card card__three">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
         </div>
         <div className="col-2 box--holder">
+          <div data-aos="fade-up" className="card card__four"></div>
           <div data-aos="fade-up" className="card card__two bg-warning">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
           <div data-aos="fade-up" className="card card__one"></div>
-          <div data-aos="fade-up" className="card card__three bg-info">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
-          </div>
-          <div data-aos="fade-up" className="card card__four"></div>
           <div data-aos="fade-up" className="card card__three bg-success">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
+          </div>
+          <div data-aos="fade-up" className="card card__three bg-info">
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
         </div>
         <div className="col-2 box--holder">
           <div data-aos="fade-up" className="card card__three bg-info">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
           <div data-aos="fade-up" className="card card__three">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
           <div data-aos="fade-up" className="card card__four"></div>
           <div data-aos="fade-up" className="card card__two bg-primary">
-            <div className="container card__img"></div>
-            <div className="text-center">Firstname</div>
+            <div className="container card__img">
+              <img src={profile} alt="zurag" />
+            </div>
+            <div className="text-center">
+              <p>Name</p>
+              <p>NUM</p>
+            </div>
           </div>
           <div data-aos="fade-up" className="card card__one"></div>
         </div>
